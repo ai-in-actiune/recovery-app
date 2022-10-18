7284520216f6f6a983e8a664d1298253036e1f66
Vasile Rotaru <vasilerotaru38@gmail.com>
2022-10-18 11:49:47 +0300

    Basic project structure.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Aplicație pentru aprecierea similarității a două secvențe distincte de mișcări ale corpului uman.',
+    author='IA în Acțiune',
+    license='GNU General Public License v3',
+)
